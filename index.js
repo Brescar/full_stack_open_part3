0c3503919998f3c82bfde15d7e315ef1f8b61625
Brescar <ghitavalentin2001@gmail.com>
2024-04-04 12:11:40 +0300

    Exercises 3.15 - 3.18 completed

--- index.js
+++ index.js
@@ -34,51 +34,37 @@ function getDate() {
   return formattedDate;
 }
 
-function getId() {
-  let id;
-  for (let i = 0; i < 10; i++) {
-    id = Math.floor(Math.random() * 10000);
-    if (!persons.find((p) => p.id === id)) {
-      break;
-    }
-    id = -1;
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      response.status(200).json(person);
+    })
+    .catch((error) => next(error));
+});
+
+app.get("/info", (request, response, next) => {
+  Person.find({})
+    .then((people) => {
+      response.send(`Phonebook has info for ${people.length} people<br/>${getDate()}`);
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+app.post("/api/persons", (request, response, next) => {
+  const body = request.body;
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
   }
-
-  return id;
-}
-
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then((persons) => response.status(200).json(persons));
-});
-
-//below not working with the DB (not yet implemented)
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  console.log("request.params.id = ", request.params.id);
-  console.log("id = ", id);
-  const person = persons.find((p) => p.id === id);
-  console.log("person = ", person);
-  if (person) {
-    response.status(200).json(person);
-  } else {
-    response.status(404).end();
-  }
-});
-
-//below not working with the DB (not yet implemented)
-app.get("/info", (request, response) => {
-  response.send(`Phonebook has info for ${persons.length} people<br/>${getDate()}`);
-});
-
-//below not working with the DB (not yet implemented)
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((p) => p.id !== id);
-
-  response.status(204).end();
-});
-
-app.post("/api/persons", (request, response) => {
   const name = request.body.name;
   const number = request.body.number;
   if (!name) {
@@ -89,19 +75,42 @@ app.post("/api/persons", (request, response) => {
     response.status(400).json({ error: "number is missing" });
     return;
   }
-  //below not working with the DB (not yet implemented)
-  // if (persons.find((p) => p.name === name)) {
-  //   response.status(400).json({ error: "name must be unique" });
-  //   return;
-  // }
 
   const person = new Person({
     name: name,
     number: number,
   });
 
-  person.save().then((savedPerson) => response.status(201).json(savedPerson));
+  person
+    .save()
+    .then((savedPerson) => response.status(201).json(savedPerson))
+    .catch((error) => next(error));
 });
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const person = {
+    name: request.body.name,
+    number: request.body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((result) => {
+      response.status(200).send(result);
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id " });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
